6eaee93a737ef07d379cf39a605c7a1635638419
ntjohns1 <nelsontjohns@gmail.com>
2021-06-30 21:08:46 -0400

    another typo

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const Category = require('./Category');
-const Products = require('./Products');
+const Products = require('./Product');
 const User = require('./User');
 
 module.exports = { Category, Product, User };
