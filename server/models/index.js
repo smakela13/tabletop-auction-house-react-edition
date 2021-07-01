88324f95ee71351eb3eff009686a33ec8e2b7951
SMakela <73409799+smakela13@users.noreply.github.com>
2021-06-30 22:09:27 -0400

    Fixed a typo in models/index.js - Added a Navbar component - Created files for Item.js and Product.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const Category = require('./Category');
-const Products = require('./Product');
+const Product = require('./Product');
 const User = require('./User');
 
 module.exports = { Category, Product, User };
