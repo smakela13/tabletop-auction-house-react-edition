604613aff83337cd818f4e90999816945156e0a2
ntjohns1 <nelsontjohns@gmail.com>
2021-06-30 21:07:13 -0400

    fixed typo in models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,4 @@ const Category = require('./Category');
 const Products = require('./Products');
 const User = require('./User');
 
-module.exports = { Category, Products, User };
+module.exports = { Category, Product, User };
