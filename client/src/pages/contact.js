28b3f62df5e2db492be44bca60a02372b734a9db
SMakela <73409799+smakela13@users.noreply.github.com>
2021-07-11 14:46:32 -0400

    Added basic form for the contact page

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -15,7 +15,16 @@ const Contact = () => {
 
     return (
         <>
-            Email form here...
+            <form className="contact-form" onSubmit={sendEmail}>
+                <input type="hidden" name="contact_number" />
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+            </form>
         </>
     );
 }
