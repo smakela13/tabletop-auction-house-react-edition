9d817399a28164916884dccfdd17b1c46a11fdc0
SMakela <73409799+smakela13@users.noreply.github.com>
2021-07-11 14:42:15 -0400

    Added logic and import for the contact page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,35 +1,41 @@
 import React from 'react';
-import { Navbar, Nav, Image } from 'react-bootstrap';
+import { Navbar, Nav, Image, Container } from 'react-bootstrap';
 
 const Navigation = () => {
 	return (
 		<>
-			<Image src='TabletopAuctionHouseNew.jpg' style={{objectFit: 'cover'}} fluid />
-			<Navbar
-				collapseOnSelect
-				sticky='top'
-				expand='sm'
-				variant='dark'
-				className='header mb-3'
-				style={{ backgroundColor: '#373737' }}>
-				<Navbar.Brand
-					className='header-nav px-3'
-					style={{ fontSize: '35px', fontFamily: 'Roboto' }}>
-					Welcome
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
-				<Navbar.Collapse id='responsive-navbar-nav'>
-					<Nav
-						className='ms-auto px-2'
-						style={{ fontSize: '20px', fontFamily: 'Roboto' }}>
-						<Nav.Link href='/'>Home</Nav.Link>
-						<Nav.Link href='/addItem'>Add Item</Nav.Link>
-						<Nav.Link href='/contact'>Contact</Nav.Link>
-						<Nav.Link href='/login'>Log In</Nav.Link>
-						<Nav.Link href='/signup'>Sign Up</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
+			<Container style={{ width: '100%',  whiteSpace: 'nowrap'}}>
+				<Image
+					src='TabletopAuctionHouseNew.jpg'
+					style={{ objectFit: 'cover', margin: '0' }}
+					fluid
+				/>
+				<Navbar
+					collapseOnSelect
+					sticky='top'
+					expand='sm'
+					variant='dark'
+					className='header mb-3'
+					style={{ backgroundColor: '#373737', margin: '0' }}>
+					<Navbar.Brand
+						className='header-nav px-3'
+						style={{ fontSize: '35px', fontFamily: 'Roboto' }}>
+						Welcome
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+					<Navbar.Collapse id='responsive-navbar-nav'>
+						<Nav
+							className='ms-auto px-2'
+							style={{ fontSize: '20px', fontFamily: 'Roboto' }}>
+							<Nav.Link href='/'>Home</Nav.Link>
+							<Nav.Link href='/addItem'>Add Item</Nav.Link>
+							<Nav.Link href='/contact'>Contact</Nav.Link>
+							<Nav.Link href='/login'>Log In</Nav.Link>
+							<Nav.Link href='/signup'>Sign Up</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+			</Container>
 		</>
 	);
 };
--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    function sendEmail(event) {
+        event.preventDefault();
+    
+        emailjs.sendForm(, , event.target, )
+        .then((result) => {
+        console.log(result.text);
+        }, (error) => {
+        console.log(error.text);
+        });
+    }
+
     return (
         <>
             Email form here...
