cb25b9959f3c027f64818380e112fdea498d3c33
SMakela <73409799+smakela13@users.noreply.github.com>
2021-07-13 15:40:32 -0400

    Restructured Navbar.js a little to have conditional rendering for Logout/Sign Up/Log In

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,83 +4,51 @@ import Auth from '../utils/auth';
 import '../App.css';
 
 const Navigation = () => {
-	if (!Auth.loggedIn()) {
-		return (
-			<>
-				<Image
-					src='TabletopAuctionHouseNew.jpg'
-					style={{ objectFit: 'cover' }}
-					fluid
-					className='navbar'
-				/>
-				<Navbar
-					collapseOnSelect
-					sticky='top'
-					expand='sm'
-					variant='dark'
-					className='header mb-5'
-					style={{ backgroundColor: '#373737' }}>
-					<Navbar.Brand
-						className='header-nav px-5'
-						style={{ fontSize: '35px', fontFamily: 'Roboto' }}>
-						Welcome
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
-					<Navbar.Collapse id='responsive-navbar-nav'>
-						<Nav
-							className='ms-auto px-2 w-100'
-							style={{ fontSize: '20px', fontFamily: 'Roboto' }}>
-							<Nav.Link href='/'>Home</Nav.Link>
-							<Nav.Link href='/addItem'>Add Item</Nav.Link>
-							<Nav.Link href='/contact'>Contact</Nav.Link>
-							<Nav.Link href='/signup'>Sign Up</Nav.Link>
-							<Nav.Link href='/login'>Log In</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-			</>
-		);
-	}
-	if (Auth.loggedIn()) {
-		return (
-			<>
-				<Image
-					src='TabletopAuctionHouseNew.jpg'
-					style={{ objectFit: 'cover' }}
-					fluid
-				/>
-				<Navbar
-					collapseOnSelect
-					sticky='top'
-					expand='sm'
-					variant='dark'
-					className='header mb-3'
-					fluid
-					style={{ backgroundColor: '#373737' }}>
-					<Navbar.Brand
+	const loggedStatus = Auth.loggedIn();
+	
+	return (
+		<>
+			<Image
+				src='TabletopAuctionHouseNew.jpg'
+				style={{ objectFit: 'cover' }}
+				fluid
+				className='navbar'
+			/>
+			<Navbar
+				collapseOnSelect
+				sticky='top'
+				expand='sm'
+				variant='dark'
+				className='header mb-5'
+				style={{ backgroundColor: '#373737' }}>
+				<Navbar.Brand
 						className='header-nav px-3'
 						style={{ fontSize: '35px', fontFamily: 'Roboto' }}>
 						Welcome, <span onClick={() => document.location.replace(`/${Auth.getProfile().data.username}`)}>{Auth.getProfile().data.username}</span>
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
-					<Navbar.Collapse id='responsive-navbar-nav'>
-						<Nav
-							className='ms-auto px-2'
-							style={{
-								fontSize: '20px',
-								fontFamily: 'Roboto',
-								color: 'white',
-							}}>
-							<Nav.Link href='/'>Home</Nav.Link>
-							<Nav.Link href='/addItem'>Add Item</Nav.Link>
-							<Nav.Link href='/contact'>Contact</Nav.Link>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+				<Navbar.Collapse id='responsive-navbar-nav'>
+					<Nav
+						className='ms-auto px-2 w-100'
+						style={{ fontSize: '20px', fontFamily: 'Roboto', color: 'white' }}>
+						<Nav.Link href='/'>Home</Nav.Link>
+						<Nav.Link href='/addItem'>Add Item</Nav.Link>
+						<Nav.Link href='/contact'>Contact</Nav.Link>
+						{loggedStatus ? (
 							<Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-				</Navbar>
-			</>
-		);
-	}
+						) : (
+							<Nav.Link href='/signup'>Sign Up</Nav.Link>
+						)}
+						{loggedStatus ? (
+							''
+						) : (
+							<Nav.Link href='/login'>Log In</Nav.Link>
+						)}
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</>
+	);
 };
 
 export default Navigation;
