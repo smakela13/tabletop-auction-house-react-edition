d2ec86602d43aef4babbd1a0670d1fe597c81593
Elise Daly <elise.samantha.daly@gmail.com>
2021-07-13 15:23:02 -0400

    Added skeleton for a profile page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import AddItem from './pages/addItem';
 import Contact from './components/Contact';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Profile from './pages/profile'
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -44,6 +45,9 @@ export default function App() {
         <div className="impDiv">
           {/* <Header /> */}
             <Navigation />
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
             <Route exact path="/" component={Product}>
               <Store />
             </Route>
