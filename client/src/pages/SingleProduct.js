0b2dd936650bcc453ad8fb5162d2c6f8e74ab984
Elise Daly <elise.samantha.daly@gmail.com>
2021-07-17 13:25:02 -0400

    Fixing singleproduct component so as to not render on all pages

--- client/src/pages/SingleProduct.js
+++ client/src/pages/SingleProduct.js
@@ -20,26 +20,32 @@ const SingleProduct = () => {
   if (loading) {
     return <div>Loading...</div>;
   }
-  return (
-    <div className="my-3">
-      <h3 className="card-header bg-dark text-light p-2 m-0">
-        {product.productName} <br />
-      </h3>
-      <div className="bg-light py-4">
-        <blockquote
-          className="p-4"
-          style={{
-            fontSize: '1.5rem',
-            fontStyle: 'italic',
-            border: '2px dotted #1a1a1a',
-            lineHeight: '1.5',
-          }}
-        >
-          {product.description}
-        </blockquote>
+
+  const url = `${document.location}`;
+
+  if (url.endsWith(product._id)) {
+    return (
+      <div className="my-3">
+        <h3 className="card-header bg-dark text-light p-2 m-0">
+          {product.productName} <br />
+        </h3>
+        <div className="bg-light py-4">
+          <blockquote
+            className="p-4"
+            style={{
+              fontSize: '1.5rem',
+              fontStyle: 'italic',
+              border: '2px dotted #1a1a1a',
+              lineHeight: '1.5',
+            }}
+          >
+            {product.description}
+          </blockquote>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  else {return (<></>)}
 };
 
 export default SingleProduct;
