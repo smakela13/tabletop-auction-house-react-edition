09defa311404118a45abd7d10aefedacdf29a003
SMakela <73409799+smakela13@users.noreply.github.com>
2021-07-17 09:20:12 -0400

    Added colons to form label on Product Form's index.js, fixed formatting, and updated CSS for consistency with Contact form - Fixed the contact form's width and formatting - Added a missing period on addItem - Added CSS for the forms in App.css - Updated README file

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -5,47 +5,50 @@ require('dotenv').config();
 const userID = `${process.env.USER_ID}`;
 
 const Contact = () => {
-    function sendEmail(event) {
-        event.preventDefault();
+	function sendEmail(event) {
+		event.preventDefault();
 
-        const contactForm = document.querySelector('#contact-form');
-    
-        emailjs
-            .sendForm(
-                `${process.env.SERVICE_ID}`,
-                `${process.env.TEMPLATE_ID}`,
-                event.target,
-                userID
-            )
-            .then(
-                (result) => {
-                    contactForm.reset();
-                    console.log(result.text);
-                },
-                (error) => {
-                    console.log(error.text);
-                }
-            );
-    }
+		const contactForm = document.querySelector('#contact-form');
 
-    return (
-			<>
-				<form className='contact-form' onSubmit={sendEmail}>
-					<label>Name:</label>
-					<input type='text' name='from_name' required />
-					<label>Email Address:</label>
-					<input
-						type='email'
-						name='reply_to'
-						pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
-						required
-					/>
-					<label>Message:</label>
-					<textarea type='text' name='message' maxLength='2500' required />
-					<input type='submit' value='Send' />
-				</form>
-			</>
-		);
-}
+		emailjs
+			.sendForm(
+				`${process.env.SERVICE_ID}`,
+				`${process.env.TEMPLATE_ID}`,
+				event.target,
+				userID
+			)
+			.then(
+				(result) => {
+					contactForm.reset();
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+	}
+
+	return (
+		<>
+			<form
+				className='contact-form'
+				style={{ width: '500px' }}
+				onSubmit={sendEmail}>
+				<label>Name:</label>
+				<input type='text' name='from_name' required />
+				<label>Email Address:</label>
+				<input
+					type='email'
+					name='reply_to'
+					pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+					required
+				/>
+				<label>Message:</label>
+				<textarea type='text' name='message' maxLength='2500' required />
+				<input type='submit' value='Send' />
+			</form>
+		</>
+	);
+};
 
 export default Contact;
