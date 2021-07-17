bcef147b053c6fe146a723ab922d8884ef5e91a6
SMakela <73409799+smakela13@users.noreply.github.com>
2021-07-17 09:33:44 -0400

    Added possibility of card columns to the store page - Removed unneeded CSS style from App.css

--- client/src/pages/store.js
+++ client/src/pages/store.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { Card, CardColumns, ListGroup } from 'react-bootstrap';
 import {  useQuery } from '@apollo/client';
 // import { REMOVE_ITEM } from '../utils/mutations';
 import { QUERY_PRODUCTS } from '../utils/queries';
@@ -10,22 +10,31 @@ const Product = () => {
         const { loading, data } = useQuery(QUERY_PRODUCTS);
         const product = data?.product || [];
 
-
-      
         if (loading) {
-          return <h2>We're loading, have some patience</h2>;
+            return <h2>We're loading; have some patience</h2>;
         }
         return (
             // <Card> ELISE: id's need to be actual ids! Idk how to do this rn
-                    <ListGroup horizontal >
+                    <ListGroup horizontal responsive >
                         <ListGroup.Item className='p-3 col-2' id={product.productId}>{product.name}</ListGroup.Item>
                         <ListGroup.Item className='p-3 col-8' id={product.productId}>{product.description}</ListGroup.Item>
                         <ListGroup.Item className='p-3 col-1' id={product.productId}>Price:<br/>{product.price}</ListGroup.Item>
                         <ListGroup.Item className='p-3 col-1' id={product.productId}>Stock:<br />{product.stock}</ListGroup.Item>
                     </ListGroup> 
-            // </Card>
+            // // </Card>
+
+            // <CardColumns>
+            //     <Card>
+            //         {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+            //         <Card.Body>
+            //             <Card.Title className='p-3 col-2' id={product.productId}>{product.name}</Card.Title>
+            //             <Card.Text id={product.productId}>{product.description}</Card.Text>
+            //             <Card.Text className='p-3 col-8' id={product.productId}>Price:<br />{product.price}</Card.Text>
+            //             <Card.Text className='p-3 col-1' id={product.productId}>Stock:<br />{product.stock}</Card.Text>
+            //         </Card.Body>
+            //     </Card>
+            // </CardColumns>
         );
-    
 }
 
 export default Product;
