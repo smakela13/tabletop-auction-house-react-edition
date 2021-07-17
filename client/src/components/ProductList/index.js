d1f9b6bfe1f1832bb2cb0092de7109ae94863816
Elise Daly <elise.samantha.daly@gmail.com>
2021-07-17 11:08:39 -0400

    Adding some work on single item queries

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -9,11 +9,11 @@ const ProductList = ({ products, title }) => {
     <Container>
       <h3>{title}</h3>
       {products && products.map((product) => (
-      <ListGroup id={product._id} key={product._id} horizontal >
-        <ListGroup.Item className='p-3 col-2' id={product.productId}>{product.productName}</ListGroup.Item>
-        <ListGroup.Item className='p-3 col-8' id={product.productId}>{product.description}</ListGroup.Item>
-        <ListGroup.Item className='p-3 col-1' id={product.productId}>Price:<br />{product.price}</ListGroup.Item>
-        <ListGroup.Item className='p-3 col-1' id={product.productId}>Stock:<br />{product.stock}</ListGroup.Item>
+      <ListGroup onClick={() => document.location.replace(`/products/${product._id}`)} key={product._id} horizontal >
+        <ListGroup.Item className='p-3 col-2'>{product.productName}</ListGroup.Item>
+        <ListGroup.Item className='p-3 col-8'>{product.description}</ListGroup.Item>
+        <ListGroup.Item className='p-3 col-1'>Price:<br />{product.price}</ListGroup.Item>
+        <ListGroup.Item className='p-3 col-1'>Stock:<br />{product.stock}</ListGroup.Item>
       </ListGroup>
       ))}
     </Container>
