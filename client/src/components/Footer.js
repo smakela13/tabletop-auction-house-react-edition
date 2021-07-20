8ed15d2cbdebfc874f087b947539704902576515
SMakela <73409799+smakela13@users.noreply.github.com>
2021-07-19 20:20:00 -0400

    Updated Footer CSS

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Image} from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+		<>
+			<footer className='footer'>
+				<p style={{fontSize: '18px'}}>Made with 🎲️ by Elise Daly, Soma Mäkelä, Thomas Musler, Nelson Johns
+					<br />
+					&copy; 2021
+				</p>
+			</footer>
+		</>
+  );
+};
+
+export default Footer;
