59a398830e733800a9408b4e428aad6498087847
Soma Makela <73409799+smakela13@users.noreply.github.com>
2021-07-20 23:53:42 -0400

    Revert "Added a message status to the Contact page"

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,22 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import emailjs from 'emailjs-com';
 require('dotenv').config();
 
-const userID = `${window.env.USER_ID}`;
+const userID = `${process.env.USER_ID}`;
 
 const Contact = () => {
-	const [statusMessage, setStatusMessage] = useState();
-
 	function sendEmail(event) {
 		event.preventDefault();
 
-		const contactForm = document.querySelector('.contact-form');
-		const statusMsg = document.querySelector('.status-message');
+		const contactForm = document.querySelector('#contact-form');
 
 		emailjs
 			.sendForm(
-				`${window.env.SERVICE_ID}`,
-				`${window.env.TEMPLATE_ID}`,
+				`${process.env.SERVICE_ID}`,
+				`${process.env.TEMPLATE_ID}`,
 				event.target,
 				userID
 			)
@@ -24,26 +21,18 @@ const Contact = () => {
 				(result) => {
 					contactForm.reset();
 					console.log(result.text);
-					setStatusMessage('Message sent!');
-					statusMsg.className = 'status-message';
-					setTimeout(() => {
-						statusMsg.className = 'status-message';
-					}, 5000);
 				},
 				(error) => {
 					console.log(error.text);
-					setStatusMessage('Failed to send message! Try again later.');
-					statusMsg.className = 'status-message';
-					setTimeout(() => {
-						statusMsg.className = 'status-message';
-					}, 5000);
 				}
 			);
 	}
 
 	return (
 		<>
-			<form className='contact-form' onSubmit={sendEmail}>
+			<form
+				className='contact-form'
+				onSubmit={sendEmail}>
 				<label>Name:</label>
 				<input type='text' name='from_name' required />
 				<label>Email Address:</label>
@@ -55,9 +44,7 @@ const Contact = () => {
 				/>
 				<label>Message:</label>
 				<textarea type='text' name='message' maxLength='2500' required />
-
-				<p className='status-message'>{statusMessage}</p>
-				<input type='submit' value='Send' style={{ color: '#fff' }} />
+				<input type='submit' value='Send' style={{color: '#fff'}} />
 			</form>
 		</>
 	);
